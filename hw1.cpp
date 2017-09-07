878f48ec369b9d6ac43ded08460be488f8cc7185
dpotter5 <31677792+dpotter5@users.noreply.github.com>
2017-09-07 16:36:36 -0600

    Update hw1.cpp

--- hw1.cpp
+++ hw1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int add1 (int input)
+{
+  int result;
+  result = input + 1;
+  return result;
+}
+
+int main ()
+{
+  int test = add1(2);
+  cout << "2 + 1 = " << test;
+}
